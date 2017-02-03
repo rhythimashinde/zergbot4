99a477826b55110785a46cf9f0be45236ff97825
sindunuragarp <sindunuragarp@gmail.com>
2017-02-03 17:59:26 +0100

    Modified build order to win against AI

--- Manager/Manager.pl
+++ Manager/Manager.pl
@@ -7,4 +7,4 @@ drone/2,
 ready/1,
 hatcheryPosition/2.
 
-allReady :- aggregate_all(count, ready(_), Count), Count > 10.
+allReady :- aggregate_all(count, ready(_), Count), Count > 5.
---
+++ ZergSpawningPool/ZergSpawningPool.pl
@@ -0,0 +1,3 @@
+:- dynamic
+
+target/2.
